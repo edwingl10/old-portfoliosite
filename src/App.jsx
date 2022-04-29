059acf786b230c944f2c77ac6340b3a1dd23c64c
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2022-04-29 10:27:18 -0700

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {
   CssBaseline,
   StyledEngineProvider,
   ThemeProvider,
   Box,
-  Typography,
 } from '@mui/material';
 import theme from './theme';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -14,8 +15,11 @@ function App() {
       <StyledEngineProvider injectFirst>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-
-          <Typography variant="h1">Website coming soon!</Typography>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Router>
         </ThemeProvider>
       </StyledEngineProvider>
     </Box>
