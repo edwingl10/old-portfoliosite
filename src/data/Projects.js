8e1b9e05937f7d056f679155ecf0652147ae4e86
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2022-06-08 23:27:35 -0700

    projects basic info and styling
    - projects data object
    - basic projects info
    - basic styling
    - responsiveness

---
+++ src/data/Projects.js
@@ -0,0 +1,76 @@
+import placeholderImg from '../img/placeholder.jpg';
+
+const Projects = {
+  DreamScholars: {
+    overview: 'short overview goes here',
+    description: '',
+    type: 'web',
+    link: '',
+    mainImg: placeholderImg,
+  },
+  'Netflix Clone': {
+    overview: 'short overview goes here',
+    description: '',
+    type: 'web',
+    link: '',
+    mainImg: placeholderImg,
+  },
+  'Cleaning Site': {
+    overview: 'short overview goes here',
+    description: '',
+    type: 'web',
+    link: '',
+    mainImg: placeholderImg,
+  },
+  'Mobile Platformer': {
+    overview: 'short overview goes here',
+    description: '',
+    type: 'game',
+    link: '',
+    mainImg: placeholderImg,
+  },
+  'Pacific Marine App': {
+    overview: 'short overview goes here',
+    description: '',
+    type: 'game',
+    link: '',
+    mainImg: placeholderImg,
+  },
+  'Game Companion app ': {
+    overview: 'short overview goes here',
+    description: '',
+    type: 'game',
+    link: '',
+    mainImg: placeholderImg,
+  },
+  'Search Engine': {
+    overview: 'short overview goes here',
+    description: '',
+    type: 'other',
+    link: '',
+    mainImg: placeholderImg,
+  },
+  'Fault Localization': {
+    overview: 'short overview goes here',
+    description: '',
+    type: 'other',
+    link: '',
+    mainImg: placeholderImg,
+  },
+  'Operating System GUI': {
+    overview: 'short overview goes here',
+    description: '',
+    type: 'other',
+    link: '',
+    mainImg: placeholderImg,
+  },
+  'Weather App': {
+    overview: 'short overview goes here',
+    description: '',
+    type: 'web',
+    link: '',
+    mainImg: placeholderImg,
+  },
+};
+
+export default Projects;
