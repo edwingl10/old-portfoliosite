104119bede3f4bdd057eff3cd966a51cd0ad3dbf
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2022-06-12 17:33:38 -0700

    route made for projects page, page top section

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Stack, Typography, Box, Container } from '@mui/material';
+import WelcomeImg from '../img/welcome.png';
+
+export default function Projects() {
+  return (
+    <Container>
+      <Stack
+        direction={{ xs: 'column-reverse', md: 'row' }}
+        columnGap="20%"
+        justifyContent="space-between"
+        sx={{ py: 5 }}>
+        <Box sx={{ mt: 4 }}>
+          <Typography variant="h3" paragraph color="secondary">
+            Projects
+          </Typography>
+          <Typography variant="h5" paragraph>
+            Check out some of the projects I have worked on, using different
+            languages and frameworks.
+          </Typography>
+        </Box>
+
+        <Box
+          component="img"
+          src={WelcomeImg}
+          alt="projects page"
+          sx={{ height: { xs: 200, sm: 250 }, m: 'auto' }}
+        />
+      </Stack>
+    </Container>
+  );
+}
